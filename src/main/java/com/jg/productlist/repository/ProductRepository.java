057ba1c3956ce144896d90a9d7ea9c1873476518
vetag <vetaga5@gmail.com>
2020-08-08 19:28:10 +0200

    #css

--- src/main/java/com/jg/productlist/repository/ProductRepository.java
+++ src/main/java/com/jg/productlist/repository/ProductRepository.java
@@ -2,8 +2,7 @@ package com.jg.productlist.repository;
 
 import com.jg.productlist.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-  //  Product findByName(String name);
+
 }
